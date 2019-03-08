ecfadeca209183944b70240066030ac0194e8989
Alejandro <alejandrehl@icloud.com>
2019-03-07 21:53:35 -0300

    Pantalla de juego terminada

---
+++ Cholor/JuegoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JuegoViewController.swift
+//  Cholor
+//
+//  Created by Alejandro Hernández Lara on 3/7/19.
+//  Copyright © 2019 Alejandro Hernández Lara. All rights reserved.
+//
+
+import UIKit
+
+class JuegoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
