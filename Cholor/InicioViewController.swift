2b032d3a2e24425ca0f2332bf5ddd2f2ddb5d737
Alejandro <alejandrehl@icloud.com>
2019-03-08 12:22:27 -0300

    Se agrego música a la app utilizando el import AVFoundation en InicioViewController

--- Cholor/InicioViewController.swift
+++ Cholor/InicioViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class InicioViewController: UIViewController {
 
+    var player = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Escuchar la música del juego
+        guard let ruta = Bundle.main.path(forResource: "Cholor", ofType: "m4a") else {
+            print("No existe la música en el proyecto")
+            return
+        }
+        
+        let url = URL(fileURLWithPath: ruta)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+            player.numberOfLoops = -1 // -1 Para que el audio se escuche siempre.
+            player.play()
+        } catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
