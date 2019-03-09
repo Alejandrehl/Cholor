b589d57f71882acf20e80f11e90b908e37571ad4
Alejandro <alejandrehl@icloud.com>
2019-03-09 18:05:45 -0300

    Agregando extension para UIColor

---
+++ Cholor/Playgrounds/UIColor+Cholor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Cholor.swift
+//  Cholor
+//
+//  Created by Alejandro Hernández Lara on 3/9/19.
+//  Copyright © 2019 Alejandro Hernández Lara. All rights reserved.
+//
+
+import UIKit
+extension UIColor {
+    static let azulFondo = UIColor(named: "Azul Fondo")
+    static let azulDestello = UIColor(named: "Azul Destello")
+}
+
+
