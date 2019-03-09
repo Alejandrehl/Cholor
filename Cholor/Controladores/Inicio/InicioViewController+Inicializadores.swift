b5ab47968bbe5e9b5f8f224b25e5d3a40959d3be
Alejandro <alejandrehl@icloud.com>
2019-03-09 18:56:06 -0300

    Se agregaron las extensiones de los view controllers

---
+++ Cholor/Controladores/Inicio/InicioViewController+Inicializadores.swift
@@ -0,0 +1,8 @@
+//
+//  InicioViewControllers+Inicializadores.swift
+//  
+//
+//  Created by Alejandro Hernández Lara on 3/9/19.
+//
+
+import Foundation
---
+++ Cholor/Controladores/Juego/JuegoViewController+Acciones.swift
@@ -0,0 +1,9 @@
+//
+//  JuegoViewController+Acciones.swift
+//  Cholor
+//
+//  Created by Alejandro Hernández Lara on 3/9/19.
+//  Copyright © 2019 Alejandro Hernández Lara. All rights reserved.
+//
+
+import Foundation
